32c0a9f53010199c2dca373bc247eb6724e5a8bc
Husnainarain2 <husnainabid9494@gmail.com>
2025-07-24 09:08:21 -0700

    bulit a practice code of javascript

---
+++ first.js
@@ -0,0 +1,6 @@
+    alert('i am inside html page')
+    console.log('hello friends');
+    console.log(2+3);
+    console.log('husnain'+'abid javed');
+    console.warn('this is a warning');
+    console.error('this is error');
